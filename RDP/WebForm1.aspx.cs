30467fbe9c11b1d104e77b52dc64963149a3c319
ayesha saiyed <aishasaiyed123@gmail.com>
2021-10-09 22:37:04 +0530

    added a new page called webform1 to upload an image file to db and preview the image file before uploading. [test]

---
+++ RDP/WebForm1.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Web;
+
+namespace RDP
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                lblMessage.Visible = false;
+                hyperlink.Visible = false;
+            }
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            HttpPostedFile postedFile = FileUpload1.PostedFile;
+            string filename = Path.GetFileName(postedFile.FileName);
+            string fileExtension = Path.GetExtension(filename);
+            int fileSize = postedFile.ContentLength;
+
+            if (fileExtension.ToLower() == ".jpg" || fileExtension.ToLower() == ".gif"
+                || fileExtension.ToLower() == ".png" || fileExtension.ToLower() == ".bmp")
+            {
+                Stream stream = postedFile.InputStream;
+                BinaryReader binaryReader = new BinaryReader(stream);
+                Byte[] bytes = binaryReader.ReadBytes((int)stream.Length);
+
+
+                string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    SqlCommand cmd = new SqlCommand("spUploadImage", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    SqlParameter paramName = new SqlParameter()
+                    {
+                        ParameterName = @"Name",
+                        Value = filename
+                    };
+                    cmd.Parameters.Add(paramName);
+
+                    SqlParameter paramSize = new SqlParameter()
+                    {
+                        ParameterName = "@Size",
+                        Value = fileSize
+                    };
+                    cmd.Parameters.Add(paramSize);
+
+                    SqlParameter paramImageData = new SqlParameter()
+                    {
+                        ParameterName = "@ImageData",
+                        Value = bytes
+                    };
+                    cmd.Parameters.Add(paramImageData);
+
+                    SqlParameter paramNewId = new SqlParameter()
+                    {
+                        ParameterName = "@NewId",
+                        Value = -1,
+                        Direction = ParameterDirection.Output
+                    };
+                    cmd.Parameters.Add(paramNewId);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+
+                    lblMessage.Visible = true;
+                    lblMessage.ForeColor = System.Drawing.Color.Green;
+                    lblMessage.Text = "Upload Successful";
+                    hyperlink.Visible = true;
+                    hyperlink.NavigateUrl = "~/WebForm2.aspx?Id=" +
+                        cmd.Parameters["@NewId"].Value.ToString();
+                }
+            }
+            else
+            {
+                lblMessage.Visible = true;
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = "Only images (.jpg, .png, .gif and .bmp) can be uploaded";
+                hyperlink.Visible = false;
+            }
+        }
+    }
+}
---
+++ RDP/WebForm2.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace RDP
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("spGetImageById", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter paramId = new SqlParameter()
+                {
+                    ParameterName = "@Id",
+                    Value = Request.QueryString["Id"]
+                };
+                cmd.Parameters.Add(paramId);
+
+                con.Open();
+                byte[] bytes = (byte[])cmd.ExecuteScalar();
+                string strBase64 = Convert.ToBase64String(bytes);
+                Image1.ImageUrl = "data:Image/png;base64," + strBase64;
+            }
+        }
+    }
+}
